6aaf0d68558d3d4a7a4ccd97e733072c68fc63d0
guopengli <liguopengbenli@gmail.com>
2021-08-25 09:27:25 +0200

    async concurrently use case waith map operator

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,17 +1,51 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentVersion = mockApi.getRecentAndroidVersions()
+                val versionsFeatures = recentVersion.map { androidVersion ->
+                    mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                }
+                uiState.value = UiState.Success(versionsFeatures)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("$e")
+            }
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
+        val features = viewModelScope.launch {
+            try {
+                val versions = mockApi.getRecentAndroidVersions()
+                // map to get a collection of deferred and await all get the list of result
+                val features = versions.map { androidVersion ->
+                    async {
+                        mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                    }
+                }.awaitAll()
+                uiState.value = UiState.Success(features)
+            } catch (e: java.lang.Exception) {
+                uiState.value = UiState.Error("$e")
+            }
+
+        }
 
     }
 }
