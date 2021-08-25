4bc3982fd591541ae2c0a0be09b5a9ce9986beba
guopengli <liguopengbenli@gmail.com>
2021-08-25 17:09:03 +0200

    coroutine Room use case

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/AndroidVersionEntity.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/AndroidVersionEntity.kt
@@ -7,6 +7,7 @@ import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
 @Entity(tableName = "androidversions")
 data class AndroidVersionEntity(@PrimaryKey val apiLevel: Int, val name: String)
 
+//nice extension function to convert list to entity
 fun List<AndroidVersionEntity>.mapToUiModelList() = map {
     AndroidVersion(it.apiLevel, it.name)
 }
