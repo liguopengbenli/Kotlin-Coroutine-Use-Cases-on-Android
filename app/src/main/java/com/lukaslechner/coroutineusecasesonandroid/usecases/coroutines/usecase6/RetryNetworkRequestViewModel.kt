416aa55cc5be0f0c6dacff469f28d5c786acd524
guopengli <liguopengbenli@gmail.com>
2021-08-25 09:53:51 +0200

    repeat use case

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,49 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import android.util.Log.e
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import timber.log.Timber.e
+import java.lang.Exception
+import java.util.*
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val numberOfRetries = 2
+            try {
+                // if we catch a exception we will leave repeat function
+                // so we need another try and catch block
+                repeat(numberOfRetries) {
+                    try {
+                        loadRecentAndroidVersion()
+                        return@launch
+                    } catch (exception: Exception) {
+                        Timber.e(exception)
+                    }
+
+                }
+                loadRecentAndroidVersion()
+
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("Network failed")
+            }
+
+        }
+    }
+
+    private suspend fun loadRecentAndroidVersion() {
+        val recentAndroidVersion = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersion)
     }
 
 }
