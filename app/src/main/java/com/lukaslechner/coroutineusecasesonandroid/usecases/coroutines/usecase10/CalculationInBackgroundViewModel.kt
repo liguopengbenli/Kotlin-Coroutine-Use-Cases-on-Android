90d5d51db00cb5e671df76be9de9f747c93d2c20
guopengli <liguopengbenli@gmail.com>
2021-08-26 10:16:20 +0200

    coroutine intensive background task use case

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -1,10 +1,48 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase10
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
-
+        // if no context specyfy, the coroutine will execute in the main thread
+        // so it execute in the foreground
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            var result: BigInteger = BigInteger.ONE
+            var resultString = ""
+            val computationDuration = measureTimeMillis {
+                result = calculateFactorialOf(factorialOf)
+            }
+            val stringConversionDuration = measureTimeMillis {
+                resultString = convertToString(result)
+            }
+            uiState.value = UiState.Success(resultString, computationDuration, stringConversionDuration)
+        }
     }
+
+    // factorial of n (n!) = 1 * 2 * 3 * 4 * ... * n
+    private suspend fun calculateFactorialOf(number: Int): BigInteger =
+        withContext(Dispatchers.Default) {
+            var factorial = BigInteger.ONE
+            for (i in 1..number) {
+                factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+            }
+            factorial
+        }
+
+    private suspend fun convertToString(number: BigInteger): String =
+        withContext(Dispatchers.Default) {
+            number.toString()
+        }
+
+
+
 }
