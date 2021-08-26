15051f1bff08f134a7961b62232bcff94ba73408
guopengli <liguopengbenli@gmail.com>
2021-08-26 10:33:02 +0200

    coroutine intensive background task use case add error handling

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -2,35 +2,42 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.math.BigInteger
 import kotlin.system.measureTimeMillis
 
-class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
+class CalculationInBackgroundViewModel(
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
+) : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
-        // if no context specyfy, the coroutine will execute in the main thread
-        // so it execute in the foreground
         uiState.value = UiState.Loading
         viewModelScope.launch {
-            var result: BigInteger = BigInteger.ONE
-            var resultString = ""
-            val computationDuration = measureTimeMillis {
-                result = calculateFactorialOf(factorialOf)
+            try {
+                var result: BigInteger = BigInteger.ZERO
+                val computationDuration = measureTimeMillis {
+                    result = calculateFactorialOf(factorialOf)
+                }
+
+                var resultString = ""
+                val stringConversionDuration = measureTimeMillis {
+                    resultString = convertToString(result)
+                }
+
+                uiState.value =
+                    UiState.Success(resultString, computationDuration, stringConversionDuration)
+            } catch (exception: Exception) {
+                UiState.Error("Error while calculating result")
             }
-            val stringConversionDuration = measureTimeMillis {
-                resultString = convertToString(result)
-            }
-            uiState.value = UiState.Success(resultString, computationDuration, stringConversionDuration)
         }
     }
 
     // factorial of n (n!) = 1 * 2 * 3 * 4 * ... * n
     private suspend fun calculateFactorialOf(number: Int): BigInteger =
-        withContext(Dispatchers.Default) {
+        withContext(defaultDispatcher) {
             var factorial = BigInteger.ONE
             for (i in 1..number) {
                 factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
@@ -39,10 +46,7 @@ class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
         }
 
     private suspend fun convertToString(number: BigInteger): String =
-        withContext(Dispatchers.Default) {
+        withContext(defaultDispatcher) {
             number.toString()
         }
-
-
-
 }
