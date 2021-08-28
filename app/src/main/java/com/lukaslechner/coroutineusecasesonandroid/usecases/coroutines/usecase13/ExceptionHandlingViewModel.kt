66506d44e2c07f9bbc8e6dd13bd18412f8fba70d
guopeng li <guopeng.li@technicolor.com>
2021-08-28 12:18:17 +0200

    exception handling use case

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
@@ -1,23 +1,77 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase13
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
-import kotlinx.coroutines.*
-import timber.log.Timber
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 
 class ExceptionHandlingViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun handleExceptionWithTryCatch() {
-
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                api.getAndroidVersionFeatures(27)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network Request failed: $e")
+            }
+        }
     }
 
     fun handleWithCoroutineExceptionHandler() {
+        uiState.value = UiState.Loading
+        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            uiState.value = UiState.Error("Network request failed!")
+        }
+        viewModelScope.launch(exceptionHandler) {
+            api.getAndroidVersionFeatures(27)
+        }
 
     }
 
     fun showResultsEvenIfChildCoroutineFails() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            // we have to add supervisor scope to make sure
+            // one job failed it will not be propagated
+            supervisorScope {
+                val oreoFeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(27)
+                }
+                val pieFeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(28)
+                }
+                val android10FeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(29)
+                }
 
+                val oreoFeatures = try {
+                    oreoFeaturesDeferred.await()
+                } catch (e: Exception) {
+                    null
+                }
+
+                val pieFeatures = try {
+                    pieFeaturesDeferred.await()
+                } catch (e: Exception) {
+                    null
+                }
+
+                val android10Features = try {
+                    android10FeaturesDeferred.await()
+                } catch (e: Exception) {
+                    null
+                }
+                // list of not null only add the item not null
+                val features = listOfNotNull(oreoFeatures, pieFeatures, android10Features)
+                uiState.value = UiState.Success(versionFeatures = features)
+
+            }
+        }
     }
 }
